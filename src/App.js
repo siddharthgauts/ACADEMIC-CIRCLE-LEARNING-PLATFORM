7d9e852b2f675c26af31442adce264f647e3e316
siddharthgauts <siddharthgauts@gmail.com>
2023-10-13 12:14:02 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css"
+import Header from "./components/common/header/Header"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import About from "./components/about/About"
+import CourseHome from "./components/allcourses/CourseHome"
+import Team from "./components/team/Team"
+import Pricing from "./components/pricing/Pricing"
+import Blog from "./components/blog/Blog"
+import Contact from "./components/contact/Contact"
+import Footer from "./components/common/footer/Footer"
+import Home from "./components/home/Home"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+  <Router>
+  <Header />
+  <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/about' element={<About />} />
+    <Route path='/courses' element={<CourseHome />} />
+    <Route path='/team' element={<Team />} />
+    <Route path='/pricing' element={<Pricing />} />
+    <Route path='/journal' element={<Blog />} />
+    <Route path='/contact' element={<Contact />} />
+  </Routes>
+  <Footer />
+</Router>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/common/footer/Footer.jsx
@@ -0,0 +1,80 @@
+import React from "react"
+import { blog } from "../../../dummydata"
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <>
+      <section className='newletter'>
+        <div className='container flexSB'>
+          <div className='left row'>
+            <h1>Newsletter - Stay tune and get the latest update</h1>
+            <span>Far far away, behind the word mountains</span>
+          </div>
+          <div className='right row'>
+            <input type='text' placeholder='Enter email address' />
+            <i className='fa fa-paper-plane'></i>
+          </div>
+        </div>
+      </section>
+      <footer>
+        <div className='container padding'>
+          <div className='box logo'>
+            <h1>ACADEMIC CIRCLE</h1>
+            <span>ONLINE EDUCATION & LEARNING</span>
+            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+
+            <i className='fab fa-facebook-f icon'></i>
+            <i className='fab fa-twitter icon'></i>
+            <i className='fab fa-instagram icon'></i>
+          </div>
+          <div className='box link'>
+            <h3>Explore</h3>
+            <ul>
+              <li>About Us</li>
+              <li>Services</li>
+              <li>Courses</li>
+              <li>Blog</li>
+              <li>Contact us</li>
+            </ul>
+          </div>
+          <div className='box link'>
+            <h3>Quick Links</h3>
+            <ul>
+              <li>Contact Us</li>
+              <li>Pricing</li>
+              <li>Terms & Conditions</li>
+              <li>Privacy</li>
+              <li>Feedbacks</li>
+            </ul>
+          </div>
+          
+          <div className='box last'>
+            <h3>Have a Questions?</h3>
+            <ul>
+              <li>
+                <i className='fa fa-map'></i>
+                Siddharth Gautam
+              </li>
+              <li>
+                <i className='fa fa-phone-alt'></i>
+                +91 7704959880
+              </li>
+              <li>
+                <i className='fa fa-paper-plane'></i>
+                siddharthgauts@gmail.com
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+      <div className='legal'>
+        <p>
+          Copyright ©2023 All rights reserved | This template is made with <i className='fa fa-heart'></i> by Siddharth
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Footer
